e7c6be7d493d280520af9a7044dcc5265c0dd5b6
alecgalba <alecgalba@gmail.com>
2017-10-12 13:35:15 -0400

    added validations for attribute presences on leagues and teams

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,2 +1,5 @@
 class League < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true, length: {maximum: 50}
+
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,5 @@
 class Team < ApplicationRecord
+  validates :name, presence: true
+  validates :mascot, presence: true
+  validates :colors, presence: true
 end
