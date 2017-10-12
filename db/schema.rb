517fad5a74c985acf68505670d98cf69704cc5e6
alecgalba <alecgalba@gmail.com>
2017-10-12 14:33:51 -0400

    modified schema with creation of join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012165938) do
+ActiveRecord::Schema.define(version: 20171012181643) do
 
   create_table "leagues", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "leagues_teams", force: :cascade do |t|
+    t.integer "league_id"
+    t.integer "team_id"
   end
 
   create_table "teams", force: :cascade do |t|
