dcf064abaf5bcb9f0d2dc8f08b9ba8c377f1275f
alecgalba <alecgalba@gmail.com>
2017-10-12 14:05:06 -0400

    added some relationships between leagues and teams

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,4 +1,6 @@
 class League < ApplicationRecord
+  has_many :teams
+  
   validates :name, presence: true
   validates :description, presence: true, length: {maximum: 50}
 
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ApplicationRecord
+  has_and_belongs_to_many :leagues
+
   validates :name, presence: true
   validates :mascot, presence: true
   validates :colors, presence: true
