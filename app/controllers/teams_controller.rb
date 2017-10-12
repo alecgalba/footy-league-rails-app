e8fff48e247d1a600381b664493653b1d6ad0a81
alecgalba <alecgalba@gmail.com>
2017-10-12 14:34:28 -0400

    trying to implement the checkboxes for adding teams to existing leagues

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -40,6 +40,7 @@ class TeamsController < ApplicationController
   # PATCH/PUT /teams/1
   # PATCH/PUT /teams/1.json
   def update
+    params[:team][:league_ids] ||=[]
     respond_to do |format|
       if @team.update(team_params)
         format.html { redirect_to @team, notice: 'Team was successfully updated.' }
@@ -69,6 +70,6 @@ class TeamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
-      params.require(:team).permit(:name, :mascot, :colors)
+      params.require(:team).permit!
     end
 end
--- app/models/league.rb
+++ app/models/league.rb
@@ -1,6 +1,6 @@
 class League < ApplicationRecord
-  has_many :teams
-  
+  has_and_belongs_to_many :teams
+
   validates :name, presence: true
   validates :description, presence: true, length: {maximum: 50}
 
