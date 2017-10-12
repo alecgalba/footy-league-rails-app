b27f5f9d2841ac8431a1c55e2fb9ecdbdd287759
alecgalba <alecgalba@gmail.com>
2017-10-12 11:43:57 -0400

    scaffolded the league models to existence

---
+++ app/views/leagues/_league.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! league, :id, :name, :description, :created_at, :updated_at
+json.url league_url(league, format: :json)
---
+++ test/system/leagues_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class LeaguesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit leagues_url
+  #
+  #   assert_selector "h1", text: "League"
+  # end
+end
