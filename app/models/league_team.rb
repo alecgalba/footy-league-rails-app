88043b2bba4070659e460c5155a2f667eb25065e
alecgalba <alecgalba@gmail.com>
2017-10-13 12:39:51 -0400

    deleted old join table that was not working

--- app/models/league_team.rb
+++
@@ -1,4 +0,0 @@
-class LeagueTeam < ActiveRecord::Base
-  belongs_to :league
-  belongs_to :team
-end
--- db/migrate/20171012181643_create_leagues_teams.rb
+++
@@ -1,8 +0,0 @@
-class CreateLeaguesTeams < ActiveRecord::Migration[5.1]
-  def change
-    create_table :leagues_teams do |t|
-      t.integer :league_id
-      t.integer :team_id
-    end
-  end
-end
