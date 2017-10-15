1b2044da0ddfff046511ee5be92d09593862b2a4
alecgalba <alecgalba@gmail.com>
2017-10-15 14:17:42 -0400

    nailing down relationship between fixtures and leagues

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,9 +1,11 @@
 class Team < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :leagues
+  has_many :fixtures
+  has_many :leagues, through: :fixtures
 
   validates :name, presence: true
   validates :mascot, presence: true
   validates :colors, presence: true
-  validates :user_id, presence: false
+
 end
