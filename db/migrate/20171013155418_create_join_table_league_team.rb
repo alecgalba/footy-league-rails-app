682dd82edf985eeeddf6be21df5a62769aa608dc
alecgalba <alecgalba@gmail.com>
2017-10-15 18:41:41 -0400

    deleted join table, but added back in after errors

--- db/migrate/20171013155418_create_join_table_league_team.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableLeagueTeam < ActiveRecord::Migration[5.1]
-  def change
-    create_join_table :leagues, :teams do |t|
-       t.index [:league_id, :team_id]
-       t.index [:team_id, :league_id]
-    end
-  end
-end
