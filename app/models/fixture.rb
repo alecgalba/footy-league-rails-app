8cf141dcd1bb9c3cbdc4f5b3d85d0e5eb6e2f5e2
alecgalba <alecgalba@gmail.com>
2017-10-15 16:40:42 -0400

    added validations for creating valid data

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -1,4 +1,9 @@
 class Fixture < ApplicationRecord
   # belongs_to :team
   # belongs_to :league
+
+  validates :team_1, presence: true
+  validates :team_2, presence: true
+  validates :date, presence: true
+  validates :time, presence: true
 end
