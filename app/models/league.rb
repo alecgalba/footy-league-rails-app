1fb67794acb7be4727315d9195c5ec1749922ac0
alecgalba <alecgalba@gmail.com>
2017-10-16 17:40:23 -0400

    deleted nested attributes associations

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,7 +2,6 @@ class League < ApplicationRecord
   has_many :fixtures
   has_many :teams, through: :fixtures
   has_and_belongs_to_many :teams
-  # accepts_nested_attributes_for :fixtures
 
   validates :name, presence: true
   validates :description, presence: true, length: {maximum: 50}
