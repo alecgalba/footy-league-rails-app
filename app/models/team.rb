290dc727204a83c494993c4bb9d066e2ddff1ae1
alecgalba <alecgalba@gmail.com>
2017-10-16 16:41:05 -0400

    all relations uncommented out

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,10 +1,11 @@
 class Team < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :leagues
-  has_many :fixtures
-  # has_many :leagues, through: :fixtures
-  accepts_nested_attributes_for :fixtures
   has_many :players
+  has_many :fixtures
+  has_many :leagues, through: :fixtures
+  has_and_belongs_to_many :leagues
+  accepts_nested_attributes_for :fixtures
+
 
   validates :name, presence: true
   validates :mascot, presence: true
