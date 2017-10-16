b8713c9182032d9a47e6b5c3d8103232c3a35efc
alecgalba <alecgalba@gmail.com>
2017-10-16 16:40:37 -0400

    tried to write over-riding methods but they were breaking the app

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -1,12 +1,13 @@
 class Fixture < ApplicationRecord
-  # belongs_to :team
-  # belongs_to :league
+  belongs_to :team
+  belongs_to :league
 
-  validates :team_1, presence: true
+  # validates :team_1, presence: true
   validates :team_2, presence: true
   validates :date, presence: true
   validates :time, presence: true
 
   scope :time, -> (time) { where time: time }
   scope :date, -> (date) { where date: date }
+
 end
