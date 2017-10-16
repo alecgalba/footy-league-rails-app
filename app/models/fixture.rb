b04b9acf40a7c1d58650e5d1781afea606a5ec3a
alecgalba <alecgalba@gmail.com>
2017-10-16 17:23:25 -0400

    modified fixture model for appropriate associations

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -3,11 +3,15 @@ class Fixture < ApplicationRecord
   belongs_to :league
 
   # validates :team_1, presence: true
-  validates :team_2, presence: true
+  validates :opponent, presence: true
   validates :date, presence: true
   validates :time, presence: true
 
   scope :time, -> (time) { where time: time }
   scope :date, -> (date) { where date: date }
+
+  def teamname
+    team_id.name
+  end
 
 end
