e13360c670c9348b6aa2dba3db6254a52e9748c6
alecgalba <alecgalba@gmail.com>
2017-10-17 10:24:37 -0400

    added class scope methods for searching players

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,9 @@ class Player < ApplicationRecord
   validates :name, presence: true
   validates :age, presence: true
   validates :position, presence: true
-  
+
   belongs_to :team
+
+  scope :age, -> (age) { where age: age }
+  scope :position, -> (position) { where position: position }
 end
