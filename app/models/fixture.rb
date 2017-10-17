f363550dc5a08d92d1726035788024dcb5bc971c
alecgalba <alecgalba@gmail.com>
2017-10-17 10:46:07 -0400

    adding authorization of editing teams

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -2,16 +2,11 @@ class Fixture < ApplicationRecord
   belongs_to :team
   belongs_to :league
 
-  # validates :team_1, presence: true
   validates :opponent, presence: true
   validates :date, presence: true
   validates :time, presence: true
 
   scope :time, -> (time) { where time: time }
   scope :date, -> (date) { where date: date }
-
-  def teamname
-    team_id.name
-  end
 
 end
