dc10f571f08d133de6e55f6cec2bbed824733953
alecgalba <alecgalba@gmail.com>
2017-10-23 08:14:57 -0400

    Changed home screen to teams index

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
 
   resources :leagues
 
-  root 'leagues#index'
+  root 'teams#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
