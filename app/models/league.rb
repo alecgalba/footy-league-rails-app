16ef69d83f404ee9c10436d0f18099162d59e178
alecgalba <alecgalba@gmail.com>
2017-11-16 12:57:54 -0500

    added AR relations to comments and users

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,6 +2,8 @@ class League < ApplicationRecord
   has_many :fixtures
   has_many :teams, through: :fixtures
   has_and_belongs_to_many :teams
+  has_many :comments
+  belongs_to :user
 
   validates :name, presence: true
   validates :description, presence: true, length: {maximum: 500}
