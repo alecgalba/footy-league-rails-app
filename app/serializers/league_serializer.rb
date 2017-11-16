ff1ae039c34181cc098581c1c7390bf3d7aae3d8
alecgalba <alecgalba@gmail.com>
2017-11-16 12:59:15 -0500

    modified league serializer to build comment list

--- app/serializers/league_serializer.rb
+++ app/serializers/league_serializer.rb
@@ -1,3 +1,18 @@
 class LeagueSerializer < ActiveModel::Serializer
   attributes :id, :name, :description
+  has_one :user
+
+
+  def comment_list
+    object.comments.map do |comment|
+      {
+        id: comment.id,
+        user: {
+          id: comment.user_id,
+          name: User.find(comment.user_id).email
+        },
+        content: comment.content
+      }
+    end
+  end
 end
