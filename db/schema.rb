323ae181c75699c1b41ff497fbad17f40e1679d9
alecgalba <alecgalba@gmail.com>
2017-11-16 13:02:32 -0500

    modified schema with migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023184848) do
+ActiveRecord::Schema.define(version: 20171116173136) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "league_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "fixtures", force: :cascade do |t|
     t.string "opponent"
@@ -29,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171023184848) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "leagues_teams", id: false, force: :cascade do |t|
