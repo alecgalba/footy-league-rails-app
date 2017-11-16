3bfaf0281d11affcfdbf735bf0da9ecfc3845de2
alecgalba <alecgalba@gmail.com>
2017-11-16 13:02:06 -0500

    built comment JS model object and added renderDisplay() function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,39 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function Comment(data) {
+  this.id = data.id;
+  this.content = data.content;
+  this.user = data.user;
+}
+
+
+
+Comment.prototype.renderDisplay = function() {
+  var html = "" ;
+  html += "<div class=\'well well-white\' id=\'comment-\' + comment.id + '\'>" +  "<strong>" + this.user.email + "</strong>" + " says: " + this.content + "</div>";
+  $("#submitted-comments").append(html);
+}
+
+$(function() {
+  $("form#new_comment").on("submit", function(event) {
+    event.preventDefault();
+    var $form = $(this);
+    var action = $form.attr("action");
+    // in order to process the comment(form data), its need to be converted from an object to a string.
+    var params = $form.serialize();
+    $.ajax({
+      url: action,
+      data: params,
+      dataType: "json",
+      method: "POST"
+    })
+    .success(function(json) {
+      $(".commentBox").val("");
+      var comment = new Comment(json);
+      comment.renderDisplay();
+
+    })
+  })
+})
