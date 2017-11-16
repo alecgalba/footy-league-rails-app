72c14d79292ae5a283a289b10fa4fbad5e3f0611
alecgalba <alecgalba@gmail.com>
2017-11-16 12:59:45 -0500

    nested comments routes within leagues

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
+
   resources :teams do
     resources :players
   end
 
-  resources :leagues
+  resources :leagues do
+    resources :comments
+  end
 
   root 'teams#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
