b9e8358af244779e0b589534a30b13dd4babf6e7
alecgalba <alecgalba@gmail.com>
2017-11-16 17:26:52 -0500

    got the dynamic comments to post without refresh

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,38 +16,39 @@
 //= require turbolinks
 //= require_tree .
 
-function Comment(data) {
-  this.id = data.id;
-  this.content = data.content;
-  this.user = data.user;
-}
-
-
-
-Comment.prototype.renderDisplay = function() {
-  var html = "" ;
-  html += "<div class=\'well well-white\' id=\'comment-\' + comment.id + '\'>" +  "<strong>" + this.user.email + "</strong>" + " says: " + this.content + "</div>";
-  $("#submitted-comments").append(html);
-}
-
 $(function() {
-  $("form#new_comment").on("submit-comment", function(event) {
-    event.preventDefault();
-    var $form = $(this);
-    var action = $form.attr("action");
-    // in order to process the comment(form data), its need to be converted from an object to a string.
-    var params = $form.serialize();
-    $.ajax({
-      url: action,
-      data: params,
-      dataType: "json",
-      method: "POST"
-    })
-    .success(function(json) {
-      $(".commentBox").val("");
-      var comment = new Comment(json);
-      comment.renderDisplay();
+  function Comment(data) {
+    this.id = data.id;
+    this.content = data.content;
+    this.user = data.user;
+  }
 
+  Comment.prototype.renderDisplay = function() {
+    var html = "" ;
+    html += "<div id=\'comment-\' + comment.id + '\'>" +  "<strong>" + this.user.email + "</strong>" + " says: " + this.content + "</div>";
+    $("#submitted-comments").append(html);
+  }
+
+  $(function() {
+    $(document).on("submit", "form#new_comment", function(event) {
+      event.preventDefault();
+      var $form = $(this);
+      var action = $form.attr("action");
+      // in order to process the comment(form data), its need to be converted from an object to a string.
+      var params = $form.serialize();
+      $.ajax({
+        url: action,
+        data: params,
+        dataType: "json",
+        method: "POST"
+      })
+      .success(function(json) {
+        $(".commentBox").val("");
+        var comment = new Comment(json);
+        comment.renderDisplay();
+
+      })
     })
   })
+
 })
