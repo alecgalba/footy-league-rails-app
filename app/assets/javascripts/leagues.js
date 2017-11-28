0b3bd9f6e2454d6a18f92555e1407c6036954c1c
alecgalba <alecgalba@gmail.com>
2017-11-28 14:29:41 -0500

    higher level abstarction for get request

--- app/assets/javascripts/leagues.js
+++ app/assets/javascripts/leagues.js
@@ -5,17 +5,20 @@ $(function(){
     //We'd love to use the HREF attribute of that link as the URL for the request
 
     //Fire some AJAX
-    $.ajax({
-      method: "GET",
-      url: this.href
-    }).success(function(response){
-      //Get the response(it's variable data)
-
+    // $.ajax({
+    //   method: "GET",
+    //   url: this.href
+    // }).success(function(response){
+    //   //Get the response(it's variable data)
+    //
+    //   $("div.comments").html(response)
+    //   //We'd really want to load the data to the DOM(append it to page)
+    // }).error(function(blergh){
+    //   alert("You Broke Me!")
+    // });
+    $.get(this.href).success(function(response){
       $("div.comments").html(response)
-      //We'd really want to load the data to the DOM(append it to page)
-    }).error(function(blergh){
-      alert("You Broke Me!")
-    });
+    })
 
     //load that response into the HTML of the page
 
